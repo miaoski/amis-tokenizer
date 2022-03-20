84961e017ebbd27d0ac94f30975fbfe1d44ba4cd
Philippe Z Lin (RD-TW) <philippe_z_lin@trendmicro.com>
2022-03-20 10:38:46 +0800

    retrieve title, stem, tag

--- collect-stem-tags.py
+++ collect-stem-tags.py
@@ -4,16 +4,17 @@
 from glob import glob
 import json
 import pickle
+from tqdm import tqdm
 
 stem_tags = {}
-for fn in glob('../amis-moedict/docs/s/*.json'):    # 請先 git clone 到 ../amis-moedict/
+fnx = glob('../amis-moedict/docs/s/*.json')     # 請先 git clone 到 ../amis-moedict/
+for fn in tqdm(fnx):
     try:
         with open(fn) as f:
             word = json.load(f)
             stem = word.get('stem')
             tag = word.get('tag')
-            if stem and tag:
-                stem_tags[stem] = tag
+            stem_tags[word['t']] = [stem, tag]
     except KeyboardInterrupt:
         break
     except:
